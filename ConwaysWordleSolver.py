4922aef8daa2615d5a9e5ec9a7f8422702c024f4
benconway03 <benjbconway@icloud.com>
2025-10-20 15:08:22 +0100

    Additional function to improve performance

--- ConwaysWordleSolver.py
+++ ConwaysWordleSolver.py
@@ -4,6 +4,45 @@ import os
 
 def wordle_solver(answer_list=[]):
 
+    letter_frequency = {
+        "E": 12.02,
+        "T": 9.10,
+        "A": 8.12,
+        "O": 7.68,
+        "I": 7.31,
+        "N": 6.95,
+        "S": 6.28,
+        "R": 6.02,
+        "H": 5.92,
+        "D": 4.32,
+        "L": 3.98,
+        "U": 2.88,
+        "C": 2.71,
+        "M": 2.61,
+        "F": 2.30,
+        "Y": 2.11,
+        "W": 2.09,
+        "G": 2.03,
+        "P": 1.82,
+        "B": 1.49,
+        "V": 1.11,
+        "K": 0.69,
+        "X": 0.17,
+        "Q": 0.11,
+        "J": 0.10,
+        "Z": 0.07
+    }
+
+    def value_word2(word):
+        word = word.upper()
+        word_score = 0
+        if len(word) != 5:
+            raise ValueError("Word must be exactly 5 letters long")
+        else:
+            for letter in word:
+                word_score += letter_frequency[letter]
+        return word_score
+
     def value_word(word):
         word = word.lower()
         word_score = 0
@@ -28,7 +67,7 @@ def wordle_solver(answer_list=[]):
 
     wordle_words_ranked = sorted(
         wordle_words,
-        key=lambda w:(value_word(w), random.random())
+        key=lambda w: (value_word(w), -value_word2(w), random.random())
     )
 
     if answer_list != [['', '']]:
